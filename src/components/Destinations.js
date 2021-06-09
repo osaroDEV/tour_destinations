ec249d5a97f2a068ed24e41ab4f051f3c2de671e
Osaro Onaiwu <Osernon@gmail.com>
2021-06-09 19:17:43 +0100

    make the app basic

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Destination from './Destination'
 
-const Destinations = ({tours}) => {
+const Destinations = ({tours, remove}) => {
   return (
     <div>
       {tours.map((tour) => {
-        return <Destination key={tour.id} {...tour} />
+        return <Destination key={tour.id} {...tour} remove={remove} />
       })}
     </div>
   )
